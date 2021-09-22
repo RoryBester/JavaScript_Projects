7675c84e028095e609fae12c455abee9a0760ceb
RoryBester <88057657+RoryBester@users.noreply.github.com>
2021-09-22 15:39:27 -0400

    java test

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,15 @@
+var A = "This is a string"; // assigning a string value
+var A = A.fontcolor("blue");//assigning a font color to the variable a
+window.alert(A);//widow alert with the variable a
+document.write('Hello, World!'//text on page
++ "and he said \"Hey that\'s mine\" ");
+var B = "concatenated" + "String";//assiagning a varabile 
+var B = B.fontcolor("red");//changing font color to red
+document.write(B);//write the string variable B
+var Family = "The Besters", Dad = "rory",//family 
+Son = "bryson",  Mom = "courtney";//more family variables 
+document.write(Dad);//disdplaying the str value of the variable dad
+var green = "i have to vommit.";//assigning a string value to the variable green
+var green = green.fontcolor("green");//using the fontcolor method to change the font to green
+document.write(green);//displaying the the string for the variable green
+document.write(3+3) //asking the computer to perform a math problem and print the results on the screen
