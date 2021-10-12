cc862e0fe9dab3d0ac39f1039c3bd6eb6793e5b2
RoryBester <88057657+RoryBester@users.noreply.github.com>
2021-10-12 15:40:24 -0400

    not and or

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,49 @@
+document.write("10" + 5);
+
+document.write(2E310);
+
+document.write(-3E310);
+
+document.write(10 > 2);
+
+document.write(10 < 2);
+
+document.write(10 == 10);
+
+document.write(3 == 10);
+
+console.log(2 + 2);
+
+console.log(10 > 2);
+
+x = 10;
+y = 10;
+document.write(x === y);
+
+w = 10;
+z = "8";
+document.write(w === z);
+
+a = 10;
+b = "10";
+document.write(a === b);
+
+f = 10;
+g = 9;
+document.write(f === g);
+
+document.write(5 > 2 && 10 > 4);
+
+document.write(5 > 10 && 10 > 4);
+
+document.write(5 > 10 || 10 > 4);
+
+document.write(5 > 10 || 10 > 20);
+
+function not_Function1() {
+    document.getElementById("Nott").innerHTML = ! (20 > 10);
+}
+
+function not_Function2() {
+    document.getElementById("Notf").innerHTML = ! (5 > 10);
+}
