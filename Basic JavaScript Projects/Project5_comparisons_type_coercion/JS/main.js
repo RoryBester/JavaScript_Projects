8afb36fb8ca903429e5cb6af0d5d2570ecd7ca59
RoryBester <88057657+RoryBester@users.noreply.github.com>
2021-10-13 10:42:29 -0400

    typeof

--- Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -1,44 +1,44 @@
-document.write("10" + 5);
+document.write(typeof "10" + 5);
 
-document.write(2E310);
+document.write(typeof 2E310);
 
-document.write(-3E310);
+document.write(typeof -3E310);
 
-document.write(10 > 2);
+document.write(typeof 10 > 2);
 
-document.write(10 < 2);
+document.write(typeof 10 < 2);
 
-document.write(10 == 10);
+document.write(typeof 10 == 10);
 
-document.write(3 == 10);
+document.write(typeof 3 == 10);
 
-console.log(2 + 2);
+console.log(typeof 2 + 2);
 
-console.log(10 > 2);
+console.log(typeof 10 > 2);
 
 x = 10;
 y = 10;
-document.write(x === y);
+document.write(typeof x === y);
 
 w = 10;
 z = "8";
-document.write(w === z);
+document.write(typeof w === z);
 
 a = 10;
 b = "10";
-document.write(a === b);
+document.write(typeof a === b);
 
 f = 10;
 g = 9;
-document.write(f === g);
+document.write(typeof f === g);
 
-document.write(5 > 2 && 10 > 4);
+document.write(typeof 5 > 2 && 10 > 4);
 
-document.write(5 > 10 && 10 > 4);
+document.write(typeof 5 > 10 && 10 > 4);
 
-document.write(5 > 10 || 10 > 4);
+document.write(typeof 5 > 10 || 10 > 4);
 
-document.write(5 > 10 || 10 > 20);
+document.write(typeof 5 > 10 || 10 > 20);
 
 function not_Function1() {
     document.getElementById("Nott").innerHTML = ! (20 > 10);
---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,5 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Can_ride = (typeof Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById(typeof "Ride").innerHTML = Can_ride + " to ride ";
+}
