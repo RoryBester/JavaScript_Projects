d25fac52bb76edec396b3669ac405dc2c670dc8f
RoryBester <88057657+RoryBester@users.noreply.github.com>
2021-10-18 16:52:56 -0400

    operators and constructors

--- Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,5 +1,29 @@
-function Ride_Function() {
-    var Height, Can_ride;
-    Can_ride = (typeof Height < 52) ? "You are too short":"You are tall enough";
-    document.getElementById(typeof "Ride").innerHTML = Can_ride + " to ride ";
+function Ride_Function() {// ride function
+    var Age, Can_vote;
+    Age = document.getElementById("Age").value; 
+    Can_vote = (Age < 18) ? "You are too young":"You are old enough";
+    document.getElementById("Can_vote").innerHTML = Can_vote + " to vote.";
+}
+function Vehicle(Make, Model, Year, Color) {//function
+    this.Vehicle_Make = Make;//this fuction make
+    this.Vehicle_Model = Model;//this function model
+    this.Vehicle_Year = Year;//this function year
+    this.Vehicle_Color = Color;//this function color
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");//variable jack
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");//variable emily
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");//variable Erik
+function myFunction() {
+    document.getElementById("New_and_This").innerHTML =
+    "Erik drives a " + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
+function count_Function() {//count function
+    document.getElementById("Nested_Function").innerHTML = Count();
+    function Count() {
+        var Starting_point = 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point; //returns the starting point number + 1
+    }
 }
